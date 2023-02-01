b7340a364e0cae2dc62b65183ed9792a5e96e7ec
Diego Pérez Gramond Muñoz <66748429+Diego-pgm@users.noreply.github.com>
2023-02-01 13:59:43 -0600

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,6 +1,3 @@
 def call(Map config = []) {
    sh "echo Hello ${config.name} today is ${config.dayOfWeek}"
-   git url "https://github.com/Diego-pgm/some_code.git"
-   sh 'chmod +x ./testscript.sh'
-   sh './testscript.sh'
 }
